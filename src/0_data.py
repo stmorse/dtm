eae44c73cc0da2a49ac9edb286263985194b6054
stmorse <steven.t.morse@gmail.com>
2025-01-13 09:09:51 -0500

    data stats script

---
+++ src/0_data.py
@@ -0,0 +1,37 @@
+import os
+import bz2
+import json
+import pickle
+import time
+
+if __name__=="__main__":
+
+    base_path = '/sciclone/data10/twford/reddit/reddit/comments'
+    save_path = '/sciclone/geograd/stmorse/reddit'
+
+    start_year = 2007
+    end_year = 2014
+    start_month = 1
+    end_month = 12
+    years = range(start_year, end_year+1)
+    months = range(start_month, end_month+1)
+    yrmo = [(yr, mo) for yr in years for mo in months]
+
+    counts = {}
+    t0 = time.time()
+    for year, month in yrmo:
+        path = os.path.join(base_path, f'RC_{year}-{month:02}.bz2')
+        c = 0
+        print(f'Counting {year}-{month} ... ({time.time()-t0:.3f})')
+        with bz2.BZ2File(path, 'rb') as f:
+            for line in f:
+                entry = json.loads(line)
+                if 'body' in entry and entry['author'] != '[deleted]':
+                    c += 1
+        counts[f'{year}-{month}'] = c
+        print(f'  Complete: {c} lines.')
+    
+    with open(os.path.join(save_path, 'line_counts.pkl'), 'w') as f:
+        pickle.dump(counts, f)
+    
+    print(f'Complete. ({time.time()-t0:.3f})')
