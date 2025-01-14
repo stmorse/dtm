8dd990bd92788588c2b955eb5c592754a34e92a4
stmorse <steven.t.morse@gmail.com>
2025-01-14 13:06:26 -0500

    reorg

--- src/2a_cluster_s.py
+++ src/2a_cluster_s.py
@@ -36,7 +36,7 @@ def train_cluster_model(
 
     print(f'CPU count                 : {joblib.cpu_count()}, {os.cpu_count()}')
     print(f'Embedding range           : {years}, {months}')
-    print(f'Saving labels to path     : {label_path}')
+    print(f'Saving labels to path     : {label_path}\n')
     
     for year, month in [(yr, mo) for yr in years for mo in months]:
         # NOTE: creating new model each month, no warm start
@@ -51,14 +51,13 @@ def train_cluster_model(
         print(f'\nLoading embeddings {year}-{month} ... ({time.time()-t0:.2f})')
         ddata = da.from_zarr(os.path.join(embed_path, f'embeddings_{year}-{month}.zarr'))
         L = ddata.shape[0]
-        M = len(ddata.chunks)
-        print(f'Total {L}, in {M} chunks')
+        print(f'Total {L}.  Chunks: {ddata.chunks}')
 
         # We'll iterate chunks and do partial fit
         i = 0
-        for chunk in ddata.to_delayed():
-            print(f'> Fitting chunk {i} ({len(chunk)}) ... ({time.time()-t0:.2f})')
+        for chunk in ddata.to_delayed().ravel():
             arr = chunk.compute()  
+            print(f'> Fitting chunk {i} ({arr.shape[0]}) ... ({time.time()-t0:.2f})')
             model.partial_fit(arr)
             i += 1
 
@@ -68,70 +67,68 @@ def train_cluster_model(
         #     np.savez_compressed(f, cc=model.cluster_centers_.copy(), allow_pickle=False)
 
         # ----
-        # LABEL
+        # LABEL + FIND top_k TO CENTROID
         # ----
+        # (need top_k for tf-idf later, 
+        #  so just do while we're doing pass over data for labels)
 
-        # labels = []
-        # print(f'Labeling (size: {L}) (chunks: {M}) ...')
-        # for chunk in ddata.to_delayed():
-        #     print(f'> Labeling chunk {i} ({len(chunk)}) ... ({time.time()-t0:.2f})')
-        #     arr = chunk.compute()
-        #     labels_chunk = model.predict(arr)
-        #     labels.append(labels_chunk)
+        print(f'Labeling (size: {L}) ...')
 
-        # labels = np.concatenate(labels)
-
-        # cluster centers are now final
-        cluster_centers = model.cluster_centers_
-
-        # label (predict) and find top_k datapoints by distance to each centroid
+        # this will hold all labels
         labels = []
-        # closest_heaps = [[] for _ in range(n_clusters)]
+
+        # find top_k datapoints by distance to each centroid
+        cluster_centers = model.cluster_centers_
+        closest_heaps = [[] for _ in range(n_clusters)]
         
         # global index to where we are in the array
-        # global_index = 0
-        for chunk in ddata.to_delayed():
+        global_index = 0
+
+        # iterate on every chunk of embedding again
+        for chunk in ddata.to_delayed().ravel():
             arr = chunk.compute()
             labels_chunk = model.predict(arr)
 
             # For each cluster c, we extract the rows in this chunk, compute distances,
             # and then keep a local top_k. We'll push that into the global top_k heap.
-            # for c in range(n_clusters):
-            #     local_indices = np.where(labels_chunk == c)[0]
-            #     if local_indices.size == 0:
-            #         continue
+            for c in range(n_clusters):
+                local_indices = np.where(labels_chunk == c)[0]
+                if local_indices.size == 0:
+                    continue
 
-            #     # Calculate distances to centroid c
-            #     dist_c = np.linalg.norm(arr[local_indices] - cluster_centers[c], axis=1)
+                # Calculate distances to centroid c
+                dist_c = np.linalg.norm(arr[local_indices] - cluster_centers[c], axis=1)
 
-            #     # Find the chunk’s top_k rows (or fewer if the cluster has <k here)
-            #     k_local = min(top_k, local_indices.size)
-            #     top_k_local = np.argpartition(dist_c, k_local)[:k_local]
+                # Find the chunk’s top_k rows (or fewer if the cluster has <k here)
+                # argpartition better than argsort here, only need top_k, unsorted
+                k_local = min(top_k, local_indices.size)
+                top_k_local = np.argpartition(dist_c, k_local)[:k_local]
 
-            #     # Insert that local top_k into the cluster's global heap
-            #     for idx in top_k_local:
-            #         dist_val = dist_c[idx]
-            #         global_idx_of_point = global_index + local_indices[idx]
+                # Insert that local top_k into the cluster's global heap
+                for idx in top_k_local:
+                    dist_val = dist_c[idx]
+                    global_idx_of_point = global_index + local_indices[idx]
 
-            #         # If heap isn't filled yet, just push
-            #         if len(closest_heaps[c]) < top_k:
-            #             heapq.heappush(closest_heaps[c], (-dist_val, global_idx_of_point))
-            #         else:
-            #             # If this new distance is better (smaller), replace the worst in the heap
-            #             worst_neg_dist, _ = closest_heaps[c][0]  # largest distance in top-k
-            #             if -dist_val > worst_neg_dist:
-            #                 heapq.heapreplace(closest_heaps[c], (-dist_val, global_idx_of_point))
+                    # If heap isn't filled yet, just push
+                    if len(closest_heaps[c]) < top_k:
+                        heapq.heappush(closest_heaps[c], (-dist_val, global_idx_of_point))
+                    else:
+                        # If this new distance is better (smaller), replace the worst in the heap
+                        worst_neg_dist, _ = closest_heaps[c][0]  # largest distance in top-k
+                        if -dist_val > worst_neg_dist:
+                            heapq.heapreplace(closest_heaps[c], (-dist_val, global_idx_of_point))
 
-            # global_index += arr.shape[0]
+            global_index += arr.shape[0]
             labels.append(labels_chunk)
 
         # --- Extract final top_k indices (sorted by distance ascending) ---
-        # closest_indices_per_cluster = {}
-        # for c in range(n_clusters):
-        #     # heap entries are (-distance, global_idx)
-        #     sorted_heap = sorted(closest_heaps[c], key=lambda x: x[0])
-        #     closest_indices_per_cluster[c] = [idx for negdist, idx in sorted_heap]
+        closest_indices_per_cluster = {}
+        for c in range(n_clusters):
+            # heap entries are (-distance, global_idx)
+            sorted_heap = sorted(closest_heaps[c], key=lambda x: x[0])
+            closest_indices_per_cluster[c] = [idx for negdist, idx in sorted_heap]
 
+        labels = np.concatenate(labels)
 
         # Save labels
         # print(f'> Saving labels ... ({time.time()-t0:.2f})')
@@ -143,9 +140,70 @@ def train_cluster_model(
         # TFIDF
         # ---
 
-        print(f'Computing tf-idf ... ')
+        print(f'Extracting text representations ... ')
 
-        print('( PASS )')
+        print(f'> Building sample corpus ... ({time.time()-t0:.3f})')
+        corpus = [[] for _ in range(n_clusters)]
+        idx_to_cluster = {ix: c for c in closest_indices_per_cluster \
+                          for ix in closest_indices_per_cluster[c]}
+        with bz2.BZ2File(os.path.join(data_path, f'RC_{year}-{month}.bz2'), 'rb') as f:
+            i = 0
+            for line in f:
+                entry = json.loads(line)
+                if 'body' not in entry or entry['author']=='[deleted]':
+                    continue
+                
+                # increment i whether we add or not
+                i += 1
+
+                # check if i is in top_k of any cluster
+                if i in idx_to_cluster:
+                    # we have a candidate
+                    c = idx_to_cluster[i]
+                    corpus[c].append(entry['body'])
+
+        
+        # collapse into a format for tf-idf vectorizor
+        for i in range(len(corpus)):
+            corpus[i] = ' --- '.join(corpus[i])
+
+        print(f'> Computing tf-idf ... ({time.time()-t0:.3f})')
+        vectorizer = TfidfVectorizer(
+            input='content',
+            max_df=max_df,
+            # max_features=100,
+            use_idf=True,
+            smooth_idf=True
+        )
+
+        X = vectorizer.fit_transform(corpus)
+
+        print(f'> Extracting top {top_m} keywords ... ({time.time()-t0:.3f})')
+        keywords = []
+        for i in range(n_clusters):
+            max_idx = np.argsort(X[i,:].toarray().flatten())[::-1][:top_m]
+            keyword = vectorizer.get_feature_names_out()[max_idx]
+            keywords.append(keyword)
+
+        print(f'> Saving output ... ({time.time()-t0:.3f})')
+        output = {
+            'year': year,
+            'month': month,
+            'full': {
+                'scores': X,
+                'feature_names': vectorizer.get_feature_names_out()
+            },
+            'tfidf': {
+                i: {
+                    'sample_indices': closest_indices_per_cluster[i],
+                    'keywords': keywords[i],
+                } for i in range(n_clusters)
+            }
+        }
+
+        # with open(os.path.join(tfidf_path, f'tfidf_{year}-{month}.pkl'), 'wb') as f:
+        #     pickle.dump(output, f)
+
 
 if __name__=="__main__":
     config = configparser.ConfigParser()
